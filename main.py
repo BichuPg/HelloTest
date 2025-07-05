64e3b2b93b6f3748be2ddd32a2af8e71de726c7e
BichuPg <115611237+BichuPg@users.noreply.github.com>
2025-07-05 15:40:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 import os
 
-SESSION_STRING = os.getenv("SESSION_STRING")
+SESSION_STRING = os.getenv("BQG9a8UAvpAuAqnt-pDQrcPUSxKLv7W5ejwdn0cRlDrJXGLSe3l8QIPEZ3UegMjlQjf1W4UDb_-8PVtr4PUX-GEyJauAvMf2v0G6-jRHBsgRuDQgos4XY8CdwzErATL0wSaqL_YKJW6O8rZnb0t6VBJBBKbmIO47ZaLPBUeWKTtNtrp45vatgqgbJvKmlps3pBw67DHwIhbcwLg_Z7wkGMIPXUUCz0LlXfQiW8JkmxSc_yQJbjXBE0AFTTzIZVyNOcgUWJTsmC4Xp5NlYMmHgTp4dTIZeKDyyIzISmMUMgNoC7l6i0jUaXA_9mgFRSTZkOL2e_Huvv2vrP8QKkGrjOoYFIHCegAAAAHJBgogAQ")
 FORWARD_TO = 15829854  # Replace with target user ID or username
 
 app = Client("forward_bot", session_string=SESSION_STRING)
